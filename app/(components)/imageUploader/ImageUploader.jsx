a402159dd85a0cbae479bd8d71cf941a94f38ed9
Nissi <olamademuyiwa@gmail.com>
2026-07-27 21:49:03 +0100

    image upload started

---
+++ app/(components)/imageUploader/ImageUploader.jsx
@@ -0,0 +1,125 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { toast } from 'sonner';
+import styles from './ImageUploader.module.css';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const ImageUploader = ({
+    value = "",
+    onUpload,
+    accept = {
+        'image/jpeg': ['.jpg', '.jpeg'],
+        'image/png': ['.png']
+    },
+    placeholder = "Upload Image",
+    maxFiles = 1,
+}) => {
+
+    const [preview, setPreview] = useState(null);
+    const [uploading, setUploading] = useState(false);
+
+    useEffect(() => {
+        return () => {
+            if (preview) URL.revokeObjectURL(preview);
+        };
+    }, [preview]);
+
+    const uploadFile = async (file) => {
+        const token = localStorage.getItem("access_token");
+        const formData = new FormData();
+        formData.append("files", file);
+        try {
+            setUploading(true);
+            const res = await fetch(`${BASE_URL}/storage/upload`, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                body: formData,
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                throw new Error(data?.message || "Upload failed");
+            }
+            toast.success("Image uploaded");
+            return data.data[0].url;
+
+        } catch (err) {
+            toast.error(err.message);
+            return null;
+        } finally {
+            setUploading(false);
+        }
+    };
+
+    const onDrop = useCallback(async (acceptedFiles) => {
+        const file = acceptedFiles[0];
+        if (!file) return;
+        setPreview(URL.createObjectURL(file));
+        const url = await uploadFile(file);
+        if (!url) return;
+        onUpload(url);
+
+    }, [onUpload]);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept,
+        multiple: false,
+        maxFiles
+
+    });
+
+    const removeImage = (e) => {
+        e.stopPropagation();
+        if (preview) {
+            URL.revokeObjectURL(preview);
+        }
+        setPreview(null);
+        onUpload("");
+    };
+
+    return (
+        <div {...getRootProps()} className={styles.container} style={{background: isDragActive ? "#f5faff" : "",}} >
+            <input {...getInputProps()} />
+            {
+                uploading ?
+                    (
+                        <div>
+                            Uploading...
+                        </div>
+                    )
+                    :
+                    preview ?
+                        (
+                            <div className={styles.previewContainer}>
+                                <img src={preview} alt="preview" className={styles.preview} />
+                                <button type="button" onClick={removeImage} className={styles.removeBtn}> × </button>
+                            </div>
+                        )
+                        :
+                        value ?
+                            (
+                                <div className={styles.previewContainer}>
+                                    <img src={value} alt="preview" className={styles.preview} />
+                                    <button type="button" onClick={removeImage} className={styles.removeBtn} > × </button>
+                                </div>
+                            )
+                            :
+                            (
+                                <div className={styles.placeholder}>
+                                    <h2>+</h2>
+                                    <p>{placeholder}</p>
+                                </div>
+                            )
+
+            }
+
+        </div>
+    );
+};
+
+export default ImageUploader;
