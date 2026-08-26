de01a645bb339cf2552d9651d4d4db91b2a46dee
Nissi <olamademuyiwa@gmail.com>
2026-08-26 22:58:08 +0100

    feat(api):vercel push erre fix 4.0

--- app/(components)/clientOrderDetail/page.jsx
+++ app/(components)/clientOrderDetail/page.jsx
@@ -55,7 +55,7 @@ const OrderDet = ({data, status}) => {
                         <p><span>Product</span></p>
                         <p><span>Cost</span></p>
                     </li>
-                    {data.items.map((b,index)=>(<li key={index}>
+                    {data?.items?.map((b,index)=>(<li key={index}>
                         <p>{b.title}</p>
                         <p><span>₦{b.price}</span></p>
                     </li>))}
@@ -63,7 +63,7 @@ const OrderDet = ({data, status}) => {
                         <p>Shipping Fee</p>
                         <p><span>₦{data.shipping_fee}</span></p>
                     </li>
-                    {data.insurance_selected && <li>
+                    {data?.insurance_selected && <li>
                         <p>Insurance</p>
                         <p><span>₦{data?.insurance_fee}</span></p>
                     </li>}
@@ -78,12 +78,12 @@ const OrderDet = ({data, status}) => {
                 </ul>
                 <div className={`${styles.shippingDet} ${styles.OrderDet}`}>
                     <div className="container">
-                        {data.delivery_method==='shiping'&&<p><span>Shipping Details</span></p>}
-                        {data.delivery_method==='pickup'&&<p><span>Pickup Details</span></p>}
-                        <p style={{marginTop:"12px"}}>{data.shipping_address || data.pickup_office}</p>
+                        {data?.delivery_method==='shiping'&&<p><span>Shipping Details</span></p>}
+                        {data?.delivery_method==='pickup'&&<p><span>Pickup Details</span></p>}
+                        <p style={{marginTop:"12px"}}>{data?.shipping_address || data?.pickup_office}</p>
                         <p>Off, Newtown Bypass way</p>
-                        <p>{data.shipping_city}, Nigeria</p>
-                        <p>Phone: {data.contact_phone}</p>
+                        <p>{data?.shipping_city}, Nigeria</p>
+                        <p>Phone: {data?.contact_phone}</p>
                         {/* <p>Email: kosiposo@mailinator.com</p> */}
                     </div>
                 </div>
